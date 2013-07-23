a00ef06be8af70a6eb18b824116d8ee53584d038
Richard Nguyen <richard_nguyen@brown.edu>
2013-07-22 23:09:03 -0400

    Separated views between sections; more optimized for viewing on iPhone 4; Added review page for previous workouts

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('fitgressus', ['ngCookies', 'fitgressus.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/index', {templateUrl: 'partials/tracker.html', controller: 'TrackerCtrl'});
+    $routeProvider.when('/index', {templateUrl: 'partials/_startWorkout.html', controller: 'StartWorkoutCtrl'});
+    $routeProvider.when('/workout', {templateUrl: 'partials/_workout.html', controller: 'WorkoutCtrl'});
+    $routeProvider.when('/exercise', {templateUrl: 'partials/_exercise.html', controller: 'ExerciseCtrl'});
+    $routeProvider.when('/finish', {templateUrl: 'partials/_end.html', controller: 'EndCtrl'});
+    $routeProvider.when('/review/:idx', {templateUrl: 'partials/_reviewWorkout.html', controller: 'ReviewWorkoutCtrl'});
     $routeProvider.otherwise({redirectTo: '/index'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,74 +3,126 @@
 /* Controllers */
 
 angular.module('fitgressus.controllers', []).
-  controller('TrackerCtrl', ['$scope', '$cookieStore', '$http', function($scope, $cookieStore, $http) {
-  	$scope.user = {
-  		name: $cookieStore.get('username') || 'User',
-  		lastCheckIn: $cookieStore.get('workouts') ? $cookieStore.get('workouts')[0].date : 'never'
-  	};
+	controller('StartWorkoutCtrl', ['$scope', '$location', '$rootScope', '$cookieStore', function($scope, $location, $rootScope, $cookieStore) {
+		$scope.date = new Date().toJSON().slice(0, 10);   //default date to today
+		$scope.previousWorkouts = $cookieStore.get('workouts');
+		$scope.beginWorkout = function () {
+			$rootScope.workout = {  // TODO: turn into service
+				date: $scope.date,
+				exercises: []
+			  };
 
-  	$scope.workoutTypes = [{
-  			label: 'Freestyle',
-  			exercises: ['Abs']
-  		}];
-  	$scope.selectedWorkoutType = $scope.workoutTypes[0];
-  	$scope.workout = {
-  		date: new Date().toJSON().slice(0, 10),		//default date to today
-  		exercises: []
-  	};
+			$rootScope.startTime = new Date().getTime() / 1000;
 
-    $scope.currentExercise = null;
+			$location.path('/workout');
+		};
 
-    $scope.newExercise = function (exerciseName) {
-        $scope.currentExercise = {
-            name: exerciseName,
-            sets: 0,
-            reps: 7,
-            wt: 0
-        };
-    };
+		$scope.removeWorkout = function (workoutIdx) {
+			$scope.previousWorkouts.splice(workoutIdx, 1);
+			$cookieStore.put('workouts', $scope.previousWorkouts);
+		};
 
-    $scope.cancelExercise = function () {
-        $scope.currentExercise = null;
-    }
+		$scope.reviewWorkout = function (workoutIdx) {
+			$location.path('/review/'+workoutIdx);
+		};
 
-    function cloneObject (obj) {
-        var clone ={};
-        for( var key in obj ){
-            if(obj.hasOwnProperty(key)) //ensure not adding inherited props
-                clone[key]=obj[key];
-        }
-        return clone;
-    }
+	}]).
+	controller('WorkoutCtrl', ['$scope', '$location', '$http', '$rootScope', function($scope, $location, $http, $rootScope) {
+		if($rootScope.workout == undefined)
+			$location.path('/index');
 
-  	$scope.addExercise = function () {
-  		$scope.workout.exercises.push(cloneObject($scope.currentExercise));
-    };
+		$scope.startExercise = function (exerciseName) {
+			$rootScope.selectedWorkoutType = $scope.selectedWorkoutType;
+			console.log($rootScope.selectedWorkoutType);
+			$rootScope.currentExercise = exerciseName;
+			$location.path('/exercise');  
+		};
 
-    $scope.dropWeight = function () {
-        if($scope.currentExercise.sets)
-            $scope.workout.exercises.push(cloneObject($scope.currentExercise));
-        $scope.currentExercise.wt = 0;
-        $scope.currentExercise.sets = 0;
-    }
+		$scope.finishWorkout = function () {
+			$location.path('/finish');	
+		};
 
-  	$scope.removeExercise = function (idx) {
-  		$scope.workout.exercises.splice(idx, 1);
-  	}
+		$http.get('data/workouts.json').success(function (data) {
+			$scope.workoutTypes = data;
+			if($rootScope.selectedWorkoutType != undefined)
+				$scope.selectedWorkoutType = $rootScope.selectedWorkoutType;
+			else
+				$scope.selectedWorkoutType = 0;
+		});
+	}]).
+	controller('ExerciseCtrl', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope) {
+		if($rootScope.workout == undefined || $rootScope.currentExercise == undefined)
+			$location.path('/index');
 
-  	$scope.saveWorkout = function () {
-  		var workouts = $cookieStore.get('workouts') || [];
-  		workouts.unshift($scope.workout);
-  		$cookieStore.put('workouts', workouts);
-  		console.log($cookieStore.get('workouts'));
-  	};
+		$scope.currentExercise = {
+			name : $rootScope.currentExercise,
+			reps : 7,
+			wt: 0,
+			sets: 0
+		  };
 
-  	$scope.emptyWorkouts = function () {
-  		$cookieStore.remove('workouts');
-  	}
+		$scope.droppedExercises = [];
 
-  	$http.get('data/workouts.json').success(function (data) {
-  		$scope.workoutTypes = $scope.workoutTypes.concat(data);
-  	});
+		var cloneObject = function (obj) {
+			var clone ={};
+			for( var key in obj ){
+				if(obj.hasOwnProperty(key)) //ensure not adding inherited props
+					clone[key]=obj[key];
+			}
+			return clone;
+		};
 
-  }]);
+		$scope.cancelExercise = function () {
+			$rootScope.currentExercise = null;
+			$location.path('/workout');
+		};
+
+		$scope.addExercise = function () {
+			if($scope.currentExercise.sets)
+				$scope.droppedExercises.push(cloneObject($scope.currentExercise));
+
+			$rootScope.workout.exercises = $rootScope.workout.exercises.concat($scope.droppedExercises);
+			$rootScope.currentExercise = null;
+			$location.path('/workout');
+		};
+
+		$scope.dropWeight = function () {
+			$scope.droppedExercises.push(cloneObject($scope.currentExercise));
+			$scope.currentExercise.wt = 0;
+			$scope.currentExercise.sets = 0;
+		};
+	}]).
+	controller('EndCtrl', ['$scope', '$location', '$rootScope', '$cookieStore', function($scope, $location, $rootScope, $cookieStore) {
+		if($rootScope.workout == undefined)
+			$location.path('/index');
+
+		var workoutTimeSecs = ((new Date().getTime() / 1000) - $rootScope.startTime);
+		var hours = parseInt(workoutTimeSecs / 3600) % 24;
+		var minutes = parseInt(workoutTimeSecs / 60) % 60;
+		var seconds = Math.round(workoutTimeSecs % 60);
+
+		$scope.duration = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+		$scope.exercises = $rootScope.workout.exercises;
+
+		$scope.removeExercise = function (exerciseIdx) {
+			$scope.exercises.splice(exerciseIdx, 1);
+		};
+
+		$scope.done = function () {
+			$rootScope.workout.duration = $scope.duration;
+
+			var workouts = $cookieStore.get('workouts') || [];
+			workouts.unshift($rootScope.workout);
+			$cookieStore.put('workouts', workouts);
+			console.log($cookieStore.get('workouts'));
+			$location.path('/index');
+		};
+	}]).
+	controller('ReviewWorkoutCtrl', ['$scope', '$routeParams', '$cookieStore', '$location', function ($scope, $routeParams, $cookieStore, $location) {
+		var workouts = $cookieStore.get('workouts');
+		var idx = $routeParams.idx;
+		if(workouts.length <= idx)
+			$location.path('/index');
+		
+		$scope.workout = workouts[idx];
+	}]);
