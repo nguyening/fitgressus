6415ad998e54f7d7557caa02e2e82ced0db3382f
Richard Nguyen <richard_nguyen@brown.edu>
2013-08-12 20:14:33 -0400

    Added contenteditable directive for two-way binding; Unnamed exercises can now be edited

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,27 @@
 'use strict';
 
 /* Directives */
+angular.module('fitgressus.directives', []).
+	directive('appVersion', ['version', function(version) {
+		return function(scope, elm, attrs) {
+			elm.text(version);
+		};
+	}]).
+	directive('contenteditable', [function() {
+		return {
+			require: 'ngModel',
+			link: function(scope, elm, attrs, ctrl) {
+				// view -> model
+				elm.bind('blur', function() {
+					scope.$apply(function() {
+						ctrl.$setViewValue(elm.html());
+					});
+				});
 
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+				// model -> view
+				ctrl.$render = function() {
+					elm.html(ctrl.$viewValue);
+				};
+			}
+		};
+	}]);
